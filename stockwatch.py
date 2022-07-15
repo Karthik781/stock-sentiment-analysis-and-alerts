04d4bde3aec0d2b91d6c61e874faffdcb55c8824
Kedam <karthik.kedam@capgemini.com>
2022-07-15 23:57:45 +0530

    filter tweets data

--- stockwatch.py
+++ stockwatch.py
@@ -1,17 +1,30 @@
+from operator import truediv
 import tweepy
 from ssm_secrets import get_secret
 
 client = tweepy.Client(
     consumer_key=get_secret("CONSUMER_KEY"),
     consumer_secret=get_secret("CONSUMER_SECRET"),
-    access_token = get_secret("ACCESS_TOKEN_KEY"),
+    access_token=get_secret("ACCESS_TOKEN_KEY"),
     access_token_secret=get_secret("ACCESS_TOKEN_SECRET")
 )
 
+
 def search_tweets(stock):
-    res = client.search_recent_tweets(stock, max_results=10, user_auth=True)
+    res = client.search_recent_tweets(stock, max_results=100, user_auth=True)
     return res
 
+
 def handler(event, context):
-    res = search_tweets()
-    print(res)
+    res = search_tweets('mirza int')
+    # print(res)
+
+    filtered = list(filter(parse_data, res.data))
+    print(filtered)
+
+
+def parse_data(text):
+    if "break out" in text:
+        return True
+    else:
+        return False
